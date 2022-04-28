2b4e0bb7fe79b34fe6d3519637f0991ef68d2113
Rylexium <aleksei.azizov@mail.ru>
2022-04-28 16:36:06 +0400

    auth new

--- src/main/java/com/sber/practika/security/JwtUserDetailsService.java
+++ src/main/java/com/sber/practika/security/JwtUserDetailsService.java
@@ -9,18 +9,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+
 
 @RequiredArgsConstructor
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
     private final UsersRepository usersRepository;
     @Override
-    public UserDetails loadUserByUsername(String bankNumber) throws UsernameNotFoundException {
-        Users user = usersRepository.findById(bankNumber).orElse(null);
-
-        if (user == null)
-            throw new UsernameNotFoundException("User with username: " + bankNumber + " not found");
-
+    public UserDetails loadUserByUsername(String param) throws UsernameNotFoundException {
+        Users user;
+        try { //isPhone
+            user = usersRepository.findByPhone(new BigInteger(param)).orElse(null);
+            if (user == null)
+                throw new UsernameNotFoundException("User with phone: " + param + " not found");
+        }catch(Exception e) { //произошла ошибка при конверте -> это username
+            user = usersRepository.findByUsername(param).orElse(null);
+            if (user == null)
+                throw new UsernameNotFoundException("User with username: " + param + " not found");
+        }
 
         return JwtUserFactory.create(user);
     }
